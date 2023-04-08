313292368e2061b2c02b8c90dcf46603c9d14784
T1mT0m <bijuwarrier1@gmail.com>
2023-04-08 07:54:15 +0530

    ProductController updated

--- ProductsService/src/main/java/com/github/bwar/eventdrivenms/estore/ProductsService/Rest/ProductController.java
+++ ProductsService/src/main/java/com/github/bwar/eventdrivenms/estore/ProductsService/Rest/ProductController.java
@@ -1,5 +1,33 @@
 package com.github.bwar.eventdrivenms.estore.ProductsService.Rest;
 
-public class ProductController {
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+	
+	@PostMapping
+	public String createProduct() {
+		return "Http POST handler";
+	}
+	
+	@GetMapping
+	public String getProduct() {
+		return "Http GET handler";
+	}
+	
+	@PutMapping
+	public String updateProduct() {
+		return "Http PUT handler";
+	}
+	
+	@DeleteMapping
+	public String deleteProduct() {
+		return "Http DELETE handler";
+	}
 }
